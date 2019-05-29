3dd01f849e64dc23e3661e8b3a14bc2e1744ff0e
baldeynz <brent.newson@baldey.net>
2019-05-30 07:09:18 +1200

    Update webhooks.go

--- webhooks.go
+++ webhooks.go
@@ -5,137 +5,155 @@ import (
 	"cloud.google.com/go/pubsub"
 	"context"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"github.com/google/go-github/github"
-	valid "github.com/asaskevich/govalidator"
 	vault "github.com/hashicorp/vault/api"
-	log "github.com/sirupsen/logrus"
+	"go.uber.org/zap" //github.com/uber-go/zap currently has an issue
+	"go.uber.org/zap/zapcore"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"os"
 	"strconv"
 	"time"
 )
 
-//Vars set by environment vars
-var (
-	projectID         string
-	retryCount        int
-	retryInterval     time.Duration
-	topicName         string
-	jenkinsWebhookUrl string
-	subName           string
-	secretPath        string
-	vaultAddress      string
-	vaultRole         string
-)
-
 const (
 	invoke   = "token"
-	k8Token  = "/var/run/secrets/kubernetes.io/serviceaccount/token"
-	K8CaCert = "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
+	K8Token  = "/var/run/secrets/kubernetes.io/serviceaccount/token"
+	K8CACert = "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
 )
 
 // global logger
-var logger = log.WithFields(log.Fields{"app": "webhooks"})
-
-//vault secret to validate payload
-var secret string
+var logger = setLogger()
 
 // client is a global Pub/Sub client, initialized once per instance.
-var client *pubsub.Client
-var ctx = context.Background()
 var topic *pubsub.Topic 
 
-func main() {
-	//get required Env Vars and fail pod start if we dont
-	//TODO - this is cumbersome, find a better way
-	projectID = os.Getenv("GCP_PROJECT")
+func isValidURL(toTest string) bool {
+    _, err := url.ParseRequestURI(toTest)
+    if err != nil {
+        return false
+    } 
+    return true   
+}
+
+//EnvironmentVars for predefined environment vars
+type EnvironmentVars struct {
+	ProjectID         string
+	JenkinsWebhookURL string
+	RetryCount        int
+	RetryInterval     time.Duration
+	SecretPath        string
+	SubName           string
+	TopicName         string
+	VaultAddress      string
+	VaultRole         string
+}
+
+//LoadVars Attemtp to load environment vars and fail if we cant
+func loadVars() *EnvironmentVars {
+	projectID := os.Getenv("GCP_PROJECT")
 	if projectID == "" {
 		logger.Fatal("Failed to get GCP_PROJECT")
 	}
 
-	topicName = os.Getenv("TOPIC_NAME")
-	if topicName == "" {
-		logger.Fatal("Failed to get TOPIC_NAME")
-	}
-
-	jenkinsWebhookUrl = os.Getenv("JENKINS_URL")
-	if jenkinsWebhookUrl == "" {
+	jenkinsWebhookURL := os.Getenv("JENKINS_URL")
+	if jenkinsWebhookURL == "" {
 		logger.Fatal("Failed to get JENKINS_URL")
 	}
-
-	if !valid.IsURL(jenkinsWebhookUrl) {
+	if !isValidURL(jenkinsWebhookURL) {
 		logger.Fatal("JENKINS_URL is invalid")
 	}
 
-	subName = os.Getenv("SUBSCRIPTION_NAME")
-	if subName == "" {
-		logger.Fatal("Failed to get SUBSCRIPTION_NAME")
-	}
-
-	secretPath = os.Getenv("VAULT_SECRET_PATH")
+	secretPath := os.Getenv("VAULT_SECRET_PATH")
 	if secretPath == "" {
 		logger.Fatal("Failed to get VAULT_SECRET_PATH")
 	}
 
-	vaultAddress = os.Getenv("VAULT_ADDRESS")
+	vaultAddress := os.Getenv("VAULT_ADDRESS")
 	if vaultAddress == "" {
 		logger.Fatal("Failed to get VAULT_ADDRESS")
 	}
 
-	vaultAddress = os.Getenv("VAULT_ADDRESS")
-	if vaultAddress == "" {
-		logger.Fatal("Failed to get VAULT_ADDRESS")
+	if !isValidURL(vaultAddress) {
+		logger.Fatal("VAULT_ADDRESS is invalid")
 	}
 
-	vaultRole = os.Getenv("VAULT_ROLE")
+	vaultRole := os.Getenv("VAULT_ROLE")
 	if vaultRole == "" {
 		logger.Fatal("Failed to get VAULT_ROLE")
+	}
+
+	subName := os.Getenv("SUBSCRIPTION_NAME")
+	if subName == "" {
+		logger.Fatal("Failed to get SUBSCRIPTION_NAME")
+	}
+
+	topicName := os.Getenv("TOPIC_NAME")
+	if topicName == "" {
+		logger.Fatal("Failed to get TOPIC_NAME")
 	}
 
 	retryAttempts, err := strconv.Atoi(os.Getenv("RETRY_COUNT"))
 	if err != nil {
 		logger.Fatal("RETRY_COUNT is invalid")
 	}
-	retryCount = retryAttempts
 
 	retryInt, err := strconv.Atoi(os.Getenv("RETRY_INTERVAL"))
 	if err != nil {
 		logger.Fatal("RETRY_INTERVAL is invalid")
 	}
-	retryInterval = time.Duration(retryInt) * time.Second
 
-	// Log as JSON instead of the default ASCII formatter.
-	log.SetFormatter(&log.JSONFormatter{})
-	// Output to stdout instead of the default stderr
-	log.SetOutput(os.Stdout)
-	// Only log the warning severity or above.
-	log.SetLevel(log.InfoLevel)
-	//we call getVaultSecret here so we dont start the pod if we fail it
-	secret = getVaultSecret()
-
-	client, err = initPubClient(projectID)
-	if err != nil {
-		logFatal(err,"Failed to initialize pub sub client")
+	return &EnvironmentVars{
+		ProjectID:         projectID,
+		JenkinsWebhookURL: jenkinsWebhookURL,
+		RetryCount:        retryAttempts,
+		RetryInterval:     time.Duration(retryInt) * time.Second,
+		SecretPath:        secretPath,
+		SubName:           subName,
+		TopicName:         topicName,
+		VaultAddress:      vaultAddress,
+		VaultRole:         vaultRole,
 	}
+}
 
-	// define a channel `eventChannel` it will send/receive hookEvents with a buffer, will block once that buffer is full
-	eventChannel := make(chan hookEvent, 100)
 
-	// fire up a new goroutine in the background for processing hook events
-	go hookProcessor(eventChannel)
+func setLogger() *zap.Logger {
+ 	atom := zap.NewAtomicLevel()
+	encoderCfg := zap.NewProductionEncoderConfig()
+	encoderCfg.TimeKey = "time"
+	encoderCfg.EncodeTime = zapcore.ISO8601TimeEncoder
 
-	srv := &http.Server{
-		Addr:         ":8090",
-		Handler:      http.HandlerFunc(processRequest(eventChannel)),
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 5 * time.Second,
-	}
+	logger := zap.New(zapcore.NewCore(
+	    zapcore.NewJSONEncoder(encoderCfg),
+	    zapcore.Lock(os.Stdout),
+	    atom,
+	))
+	defer logger.Sync()
 
-	logger.Info("Server Started")
-	logger.Fatal(srv.ListenAndServe())
+	return logger
+}
+
+func logInfo(msg string, key string, val string) {
+	logger.Info(msg,
+		zap.String(key, val),
+		zap.String("app", "webhooks"),
+	)
+}
+
+func logFatal(e error, desc string) {
+	logger.Fatal(desc,
+    	zap.Error(e),
+    	zap.String("app", "webhooks"),
+    )
+}
+
+func logError(e error, desc string) {
+	logger.Error(desc,
+		zap.Error(e),
+		zap.String("app", "webhooks"),
+    )
 }
 
 type hookEvent struct {
@@ -145,50 +163,51 @@ type hookEvent struct {
 }
 
 // hookProcessor takes the receive side of a channel
-func hookProcessor(eventCh <-chan hookEvent) {
+func hookProcessor(ctx context.Context, client *pubsub.Client, envars *EnvironmentVars, eventCh <-chan hookEvent) {
 	// Ranging over a channel will read events sent to the channell
 	// or block if there's no events to read.
 	for event := range eventCh {
-		logger.WithFields(log.Fields{"event-type": event.gitHubEvent}).Info("Recieved a new event to process")
-
+		logInfo("Recieved a new event to process", "event-type",event.gitHubEvent)
+		
 		//validate we can parse the webhook
 		gEvent, err := github.ParseWebHook(event.gitHubEvent, event.payload)
 		if err != nil {
 			logError(err,"Error process parsing incoming webhook")
-		} else {
-			switch gEvent.(type) {
-			case *github.PushEvent:
-				//try and send on to Jenkins
-				webhookSenderr := retry(retryCount, retryInterval, func() (err error) {
-					JenkinsErr :=  sendToJenkins(event.payload, event.gitHubEvent, jenkinsWebhookUrl + "?token=" + event.invokeKey)
-					if JenkinsErr != nil {
-						logError(JenkinsErr, "Jenkins Transport Error")
-						return JenkinsErr
-					}
-					return nil
-				})
-				//if jenkins send/retries are exhausted then we need to add the X-Github-Event and Token to the message and send to Topic
-				if webhookSenderr != nil {
-					logger.WithFields(log.Fields{"error": webhookSenderr}).Error("Something went wrong with webhook forward to Jenkins. Sending to topic")
-					contentPlus, constructErr := constructPubSubMsg(event.payload, event.gitHubEvent, event.invokeKey)
-					if constructErr != nil {
-						logError(constructErr,"Pubsub msg construction error")
-					}
-					err := sendToTopic(contentPlus, client, topicName)
-					if err != nil {
-						logFatal(err, "Could not publish message to topic")
-					}
-				} else { //log successful Jenkins send
-					logger.WithFields(log.Fields{"jenkins-url": jenkinsWebhookUrl + "?token=" + event.invokeKey}).Info("Webhook forwarded to Jenkins")
+		    continue
+		}
+		switch gEvent.(type) {
+		case *github.PushEvent:
+			//try and send on to Jenkins
+			webhookSendErr := retry(envars.RetryCount, envars.RetryInterval, func() (err error) {
+				JenkinsErr :=  sendToJenkins(event.payload, event.gitHubEvent, envars.JenkinsWebhookURL + "?token=" + event.invokeKey)
+				if JenkinsErr != nil {
+					logError(JenkinsErr, "Jenkins Transport Error")
+					return JenkinsErr
 				}
-			default:
-				logger.WithFields(log.Fields{"event-type": event.gitHubEvent}).Info("Currently we ignore this event-type")
+				return
+			})
+			//if jenkins send/retries are exhausted then we need to add the X-Github-Event and Token to the message and send to Topic
+			if webhookSendErr != nil {
+				logError(webhookSendErr,"Something went wrong with webhook forward to Jenkins. Sending to topic")
+				contentPlus, constructErr := constructPubSubMsg(event.payload, event.gitHubEvent, event.invokeKey)
+				if constructErr != nil {
+					logError(constructErr,"Pubsub msg construction error")
+				}
+				
+				err := sendToTopic(ctx, client, contentPlus, envars.TopicName)
+				if err != nil {
+					logFatal(err, "Could not publish message to topic")
+				}
+			} else { //log successful Jenkins send
+				logInfo("Webhook forwarded to Jenkins", "jenkins-url",envars.JenkinsWebhookURL + "?token=" + event.invokeKey)
 			}
+		default:
+			logInfo("Currently we ignore this event-type", "event-type", event.gitHubEvent)
 		}
 	}
 }
 
-func processRequest(eventCh chan hookEvent) http.HandlerFunc {
+func processRequest(secret string, eventCh chan hookEvent) http.HandlerFunc {
 	// Return a http.HandlerFunc that implements the handler functionality
 	return func(w http.ResponseWriter, r *http.Request) {
 		//Check to see if we have a relevant invoke query
@@ -199,8 +218,8 @@ func processRequest(eventCh chan hookEvent) http.HandlerFunc {
 			http.Error(w, "Error validating request", http.StatusBadRequest)
 			return
 		}
-		logger.WithFields(log.Fields{"token": keys[0]}).Info("Received webhook")
-
+		logInfo("Received webhook", "token", keys[0])
+	
 		//validate that our secret key matches what is in the headers
 		content, err := github.ValidatePayload(r, []byte(secret))
 		if err != nil {
@@ -208,16 +227,21 @@ func processRequest(eventCh chan hookEvent) http.HandlerFunc {
 			http.Error(w, "Error validating request body", http.StatusBadRequest)
 			return
 		}
-		// Instantiate a hookEvent{} and send it to the channel
-		eventCh <- hookEvent{
-			payload:     content,
-			gitHubEvent: r.Header.Get("X-Github-Event"),
-			invokeKey:   keys[0],
-		}
-
-		// Return status code and we're done
-		w.WriteHeader(http.StatusAccepted)
-		r.Body.Close()
+		select {
+			// Instantiate a hookEvent{} and send it to the channel unless its full
+			case eventCh <- hookEvent{
+					payload:     content,
+					gitHubEvent: r.Header.Get("X-Github-Event"),
+					invokeKey:   keys[0],
+				}:
+				// Return status code and we're done
+				w.WriteHeader(http.StatusAccepted)
+				r.Body.Close()
+			default: 
+				//buffer is full so send a 503
+				w.WriteHeader(http.StatusServiceUnavailable)
+				r.Body.Close()
+        }		
 	}
 }
 
@@ -248,7 +272,10 @@ func sendToJenkins(content []byte, githubEvent string, url string) (error) {
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("X-Github-Event", githubEvent)
 
-	client := &http.Client{}
+	client := &http.Client{
+		Timeout: 15 * time.Second,
+	}
+
 	response, err := client.Do(req)
 	if err != nil {
 		return err
@@ -257,30 +284,25 @@ func sendToJenkins(content []byte, githubEvent string, url string) (error) {
 
 	//test we are getting the correct status code
 	if response.StatusCode != http.StatusOK {
-		return errors.New(strconv.Itoa(response.StatusCode))
+		return fmt.Errorf("%d", response.StatusCode)
 	}
 
 	return nil
 }
 
-func sendToTopic(c []byte, psclient *pubsub.Client, topicName string) (error) {
+func sendToTopic(ctx context.Context, psclient *pubsub.Client,c []byte,  topicName string) error {
 	//publish the message
 	m := &pubsub.Message{
 		Data: []byte(c),
 	}
-	//create the topic if it doesnt exist - for testing purposes
-    topic, err := psclient.CreateTopic(ctx, topicName)
-	if err != nil {
-		logError(err, "Topic already exists, creation failed")
-		topic = psclient.Topic(topicName)
-	}
+	topic = psclient.Topic(topicName)
 	id, err := topic.Publish(ctx, m).Get(ctx)
 	if err != nil {
-		logger.WithFields(log.Fields{"topic": topic, "error": err}).Error("Topic publish error")
-		logger.WithFields(log.Fields{"message": string(c)}).Error("Topic publish error")
+		logError(err, "Topic publish error on topic: " + topicName )
+		logError(err, "Topic publish error original msg:: " + string(c))
 		return err
 	}
-	logger.WithFields(log.Fields{"message-id": id}).Info("Message sent to topic successfully")
+	logInfo("Message sent to topic successfully", "message-id", id)
 	return nil
 }
 
@@ -296,63 +318,76 @@ func retry(attempts int, sleep time.Duration, f func() error) (err error) {
 		}
 
 		time.Sleep(sleep)
-		logger.WithFields(log.Fields{"error": err, "attempts": i + 1}).Error("Retrying attempt after error")
-
+		logInfo("Retry needed", "current-attempts", strconv.Itoa(i+1))
 	}
 	return fmt.Errorf("after %d attempts, last error: %s", attempts, err)
 }
 
-func initPubClient(project string) (*pubsub.Client, error) {
-	// err is pre-declared to avoid shadowing client.
-	var err error
-	newClient, err := pubsub.NewClient(ctx, project)
+func getVaultSecret(envars *EnvironmentVars) (secret string) {
+	jwt, err := ioutil.ReadFile(K8Token)
 	if err != nil {
-		return nil, err
+		logFatal(err, "JWT Token")
 	}
-    return newClient, nil
-}
-
-func getVaultSecret() (secret string) {
-	jwt, err := ioutil.ReadFile(k8Token)
-	logFatal(err, "JWT Token")
-
 	//Set Vault Address
 	vaultConfig := &vault.Config{
-		Address: vaultAddress,
+		Address: envars.VaultAddress,
 	}
 
 	//Set CA Cert location
 	tlsConfig := &vault.TLSConfig{
-		CACert: K8CaCert,
+		CACert: K8CACert,
 	}
 
 	tlserr := vaultConfig.ConfigureTLS(tlsConfig)
-	logFatal(tlserr, "Vault TLS Config")
-
+	if err != nil {
+		logFatal(tlserr, "Vault TLS Config")
+	}
 	client, err := vault.NewClient(vaultConfig)
-	logFatal(err, "Vault Client Initiation")
-
-	auth, err := client.Logical().Write("auth/kubernetes/login", map[string]interface{}{"jwt": string(jwt), "role": vaultRole})
-	logFatal(err, "Vault Auth")
-
+	if err != nil {
+		logFatal(err, "Vault Client Initiation")
+	}
+	auth, err := client.Logical().Write("auth/kubernetes/login", map[string]interface{}{"jwt": string(jwt), "role": envars.VaultRole})
+	if err != nil {
+		logFatal(err, "Vault Auth")
+	}
 	client.SetToken(auth.Auth.ClientToken)
 
-	secretValues, err := client.Logical().Read(secretPath)
-	logFatal(err, "Vault Get Secret")
-
+	secretValues, err := client.Logical().Read(envars.SecretPath)
+	if err != nil {
+		logFatal(err, "Vault Get Secret")
+	}
 	logger.Info("Vault Secret Successfully Retrieved")
 	return fmt.Sprint(secretValues.Data["value"])
 
 }
 
-func logFatal(e error, desc string) {
-	if e != nil {
-		logger.WithFields(log.Fields{"error": e}).Fatal(desc)
-	}
-}
 
-func logError(e error, desc string) {
-	if e != nil {
-		logger.WithFields(log.Fields{"error": e}).Error(desc)
+func main() {
+	//Load environment vars
+	envars := loadVars()
+	//we call getVaultSecret here so we dont start the pod if we fail it
+	secret := getVaultSecret(envars)
+
+    //Iniate pubsub client
+    ctx := context.Background()
+	psclient, err := pubsub.NewClient(ctx, envars.ProjectID)
+	if err != nil {
+		logFatal(err, "pubsub client creation error")
 	}
+
+	// define a channel `eventChannel` it will send/receive hookEvents with a buffer, will block once that buffer is full
+	eventChannel := make(chan hookEvent, 100)
+
+	// fire up a new goroutine in the background for processing hook events
+	go hookProcessor(ctx, psclient, envars, eventChannel)
+
+	srv := &http.Server{
+		Addr:         ":8090",
+		Handler:      http.HandlerFunc(processRequest(secret, eventChannel)),
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+
+	logger.Info("Server Started")
+	logFatal(srv.ListenAndServe(),"Server Error")
 }
